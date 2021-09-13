7142e80e0c5059ffb62db316eac3d5635da05487
Moez Baccouche <moez_bac@hotmail.com>
2021-09-13 16:30:50 +0100

    Example updated

--- example/App.js
+++ example/App.js
@@ -6,15 +6,25 @@
  * @flow strict-local
  */
 
-import React from 'react';
-import type {Node} from 'react';
-import {Button, SafeAreaView, ScrollView, StatusBar, View} from 'react-native';
-import Pax from 'react-native-pax-test-moez';
+import React from "react";
+import type { Node } from "react";
+import {
+  Button,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  View,
+} from "react-native";
+import Pax from "react-native-pax-library";
 
 const App = () => {
+  const onPrint = () => {
+    Pax.printStr("Hello World !", Pax.PARTIAL_CUT);
+  };
+
   return (
-    <View style={{flex: 1}}>
-      <Button title="Click ME" onPress={() => Pax.printStr('Hello World', 0)} />
+    <View style={{ flex: 1 }}>
+      <Button title="Print" onPress={onPrint} />
     </View>
   );
 };
